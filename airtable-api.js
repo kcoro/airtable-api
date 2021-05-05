f4c7580bc1bccbf815af88ef6ad99c75c6085a08
Kevin Coronato <6244044+kcoro@users.noreply.github.com>
2021-05-04 20:50:16 -0400

    added synchronous behavior to query and prompt

--- airtable-api.js
+++ airtable-api.js
@@ -1,35 +1,32 @@
 const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY
-let Airtable = require('airtable');
-let corDistributionBase = new Airtable({apiKey: AIRTABLE_API_KEY}).base('appLhC9McNSEeDMmE');
+const Airtable = require('airtable');
+const corDistributionBase = new Airtable({apiKey: AIRTABLE_API_KEY}).base('appLhC9McNSEeDMmE');
+const table = corDistributionBase('Customer Pipeline');
+const readline = require('readline-sync');
 
 let repsCustomers = []  // list of a given sales reps customers
 let totalAnnualRevenue = 0  // total revenue for a particular sales reps account base
 let searchSalesID = -1  // reps SalesID, received from stdin
+let records = []
 
 // queryCustomerPipeline() connects to a Airtable base, and performs a table level select opera
 async function queryCustomerPipeline() {
-    corDistributionBase('Customer Pipeline').select({
-        fields: ["CustomerID", "SalesID (from Territory Manager)",  "Company Name", "Annual Revenue", "Status"],
-        maxRecords: 100,
-        view: "Grid-View-Pipeline"
-    }).eachPage(function page(records, fetchNextPage) {
-        // This function (`page`) will get called for each page of records.
-        records.forEach(record => {
-            let sid = record.get('SalesID (from Territory Manager)')
-            if (sid == searchSalesID) { repsCustomers.push(record) }
+    repsCustomers = []
+    records = await table
+        .select({
+            fields: ["CustomerID", "SalesID (from Territory Manager)",  "Company Name", "Annual Revenue", "Status"],
+            maxRecords: 100, 
+            view: 'Grid-View-Pipeline'
         })
-        // To fetch the next page of records, call `fetchNextPage`.
-        // If there are no more records, `done` will get called.
-        fetchNextPage();
-    
-    }, function done(err) {
-        if (err) { 
-            console.error(err);
-            return; 
-        }
+        .firstPage()
+
+    records.forEach(record => {
+        let sid = record.get('SalesID (from Territory Manager)')
+        if (sid == searchSalesID) { repsCustomers.push(record) }
     })
 }
 
+// Output results of querying Customer Pipeline
 function printCustomers() {
     console.log(`\nShowing accounts assigned to SalesID ${searchSalesID}\n`)
     console.log(`CID:\tCompany Name:\tAnnual Revenue:\tStatus:`)
@@ -42,23 +39,33 @@ function printCustomers() {
     console.log('\t\t\t$' + totalAnnualRevenue.toLocaleString('en-US') +'\n')
 }
 
-// Read from stdin
-const readline = require('readline-sync');
+async function main() {
+    let prompt = true
+    console.log('*********************************\n* CUSTOMER PIPELINE CLI EDITION *\n*********************************\n')
 
-let selection = readline.question(`Customer Pipeline CLI Edition
-Please select from the following options:
+    while (prompt) {
+
+        let selection = readline.question(`Please select from the following options:
 1 - Get customer list by SalesID
 2 - Update customer status
-3 - Exit
+3 - Exit\n
 Selection: `);
-
-if (selection == 1) {
-    // Prompt user to enter SalesID
-    // Run query on DB and print results of query
-    searchSalesID = readline.question('Enter SalesID: ')
-
-    if (searchSalesID != -1) {
-        queryCustomerPipeline()
-        setTimeout(() => printCustomers(), 1250)
-    } 
+            
+        if (selection == 1) {
+            // Prompt user to enter SalesID
+            // Run query on DB and print results of query
+            searchSalesID = readline.question('Enter SalesID: ')
+    
+            if (searchSalesID != -1) {
+                await queryCustomerPipeline()
+                printCustomers()
+                totalAnnualRevenue = 0
+            }
+        } else if (selection == 3) {
+            prompt = false
+        }
+    }
 }
+
+
+main()
